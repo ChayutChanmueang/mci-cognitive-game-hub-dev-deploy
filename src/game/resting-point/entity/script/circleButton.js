fbdd54f3521b6e28e90160dfae76aa8d76774770
Chayut Chanmueang <chayut.chanmueang@gmail.com>
2026-05-08 11:26:14 +0700

    feat: change resting point layout and add more feedback to resting point minigame

--- src/game/resting-point/entity/script/circleButton.js
+++ src/game/resting-point/entity/script/circleButton.js
@@ -9,10 +9,10 @@ export default class CircleButton extends Entity{
 
         this.radius = radius;
         const _buttonBorder = scene.add.graphics()
-        _buttonBorder.fillStyle(0xffffff,1);
-        _buttonBorder.fillCircle(this.x,this.y,this.radius + 15);
+        _buttonBorder.fillStyle(0x000000,1);
+        _buttonBorder.fillCircle(this.x,this.y,this.radius + 10);
         const _button = scene.add.graphics()
-        _button.fillStyle(0xff0000,1);
+        _button.fillStyle(0xd9d9d9,1);
         _button.fillCircle(this.x,this.y,this.radius);
 
         this.hitbox = new CircleHitbox(this,{radius: this.radius});
