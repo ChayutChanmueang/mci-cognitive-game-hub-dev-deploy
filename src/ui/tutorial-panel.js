005d2867bd5f1078f0444e0d51c564fd0dcf8aa3
Chayut Chanmueang <chayut.chanmueang@gmail.com>
2026-05-26 13:47:39 +0700

    feat : change art asset for tutorial panel

--- src/ui/tutorial-panel.js
+++ src/ui/tutorial-panel.js
@@ -26,7 +26,7 @@ export class TutorialPanel {
                 </div>
                 <div style="
                     position: absolute;
-                    top: 250px;
+                    top: 280px;
                     left: 50%;
                     transform: translateX(-50%);
                     font-family: 'Noto Looped Thai', sans-serif;
@@ -37,7 +37,7 @@ export class TutorialPanel {
                 ">เกมคัดเลือกอาหารให้ถูกต้อง</div>
                 <div style="
                     position: absolute;
-                    top: 322px;
+                    top: 352px;
                     left: 50%;
                     transform: translateX(-50%);
                     font-family: 'Noto Looped Thai', sans-serif;
@@ -48,7 +48,7 @@ export class TutorialPanel {
                 ">เลือกอาหารที่สัตว์กินไม่ได้ออกจากสายพาน</div>
                 <div style="
                     position: absolute;
-                    top: 404px;
+                    top: 434px;
                     left: 50%;
                     transform: translateX(-50%);
                     width: 774px;
@@ -79,6 +79,108 @@ export class TutorialPanel {
                         <img src="assets/zoo-feeder/animal/icons/H_Cow.png" style="width: 220px; height: 220px; object-fit: contain;" />
                         <img src="assets/zoo-feeder/animal/icons/H_ele.png" style="width: 220px; height: 220px; object-fit: contain;" />
                         <img src="assets/zoo-feeder/animal/icons/H_Pan.png" style="width: 220px; height: 220px; object-fit: contain;" />
+                    </div>
+                    <div style="position: absolute; top: 280px; left: 67px; width: 160px; height: 70px;">
+                        <svg width="160" height="70" viewBox="0 0 160 70" preserveAspectRatio="none" style="position: absolute; top: 0; left: 0;">
+                            <path d="M 15 0 L 130 0 C 138 0, 142 3, 147 10 L 156 25 C 160 31, 160 39, 156 45 L 147 60 C 142 67, 138 70, 130 70 L 15 70 C 6.7 70, 0 63.3, 0 55 L 0 15 C 0 6.7, 6.7 0, 15 0 Z" fill="#6A9A4E" />
+                        </svg>
+                        <div style="position: absolute; top: 0; left: 0; width: 145px; height: 70px; display: flex; align-items: center; justify-content: center; color: white; font-family: 'Noto Looped Thai', sans-serif; font-size: 36px; font-weight: 700;">กินได้</div>
+                    </div>
+                    <div style="position: absolute; top: 265px; left: 257px; display: flex; align-items: center; gap: 15px; height: 100px;">
+                        <img src="assets/zoo-feeder/food/Apple.png" style="height: 100px; width: auto; object-fit: contain;" />
+                        <img src="assets/zoo-feeder/food/Corn.png" style="height: 100px; width: auto; object-fit: contain;" />
+                        <img src="assets/zoo-feeder/food/Plant.png" style="height: 100px; width: auto; object-fit: contain;" />
+                    </div>
+                </div>
+                <div style="
+                    position: absolute;
+                    top: 850px;
+                    left: 50%;
+                    transform: translateX(-50%);
+                    width: 774px;
+                    height: 386px;
+                    background-color: #FFFBF7;
+                    border: 3px solid #F9A347;
+                    border-radius: 50px;
+                    box-sizing: border-box;
+                ">
+                    <div style="
+                        position: absolute;
+                        top: 50px;
+                        left: 65px;
+                        font-family: 'Noto Looped Thai', sans-serif;
+                        font-size: 56px;
+                        color: #F9A347;
+                        font-weight: 500;
+                        white-space: nowrap;
+                    ">สัตว์กินเนื้อ</div>
+                    <div style="
+                        position: absolute;
+                        top: 80px;
+                        width: 100%;
+                        display: flex;
+                        justify-content: center;
+                        gap: 20px;
+                    ">
+                        <img src="assets/zoo-feeder/animal/icons/H_Bear.png" style="width: 220px; height: 220px; object-fit: contain;" />
+                        <img src="assets/zoo-feeder/animal/icons/H_Fox.png" style="width: 220px; height: 220px; object-fit: contain;" />
+                        <img src="assets/zoo-feeder/animal/icons/H_Li.png" style="width: 220px; height: 220px; object-fit: contain;" />
+                    </div>
+                    <div style="position: absolute; top: 280px; left: 67px; width: 160px; height: 70px;">
+                        <svg width="160" height="70" viewBox="0 0 160 70" preserveAspectRatio="none" style="position: absolute; top: 0; left: 0;">
+                            <path d="M 15 0 L 130 0 C 138 0, 142 3, 147 10 L 156 25 C 160 31, 160 39, 156 45 L 147 60 C 142 67, 138 70, 130 70 L 15 70 C 6.7 70, 0 63.3, 0 55 L 0 15 C 0 6.7, 6.7 0, 15 0 Z" fill="#F9A347" />
+                        </svg>
+                        <div style="position: absolute; top: 0; left: 0; width: 145px; height: 70px; display: flex; align-items: center; justify-content: center; color: white; font-family: 'Noto Looped Thai', sans-serif; font-size: 36px; font-weight: 700;">กินได้</div>
+                    </div>
+                    <div style="position: absolute; top: 265px; left: 257px; display: flex; align-items: center; gap: 15px; height: 100px;">
+                        <img src="assets/zoo-feeder/food/Beef.png" style="height: 100px; width: auto; object-fit: contain;" />
+                        <img src="assets/zoo-feeder/food/Chick.png" style="height: 100px; width: auto; object-fit: contain;" />
+                        <img src="assets/zoo-feeder/food/Fish.png" style="height: 100px; width: auto; object-fit: contain;" />
+                    </div>
+                </div>
+                <div style="
+                    position: absolute;
+                    top: 1266px;
+                    left: 50%;
+                    transform: translateX(-50%);
+                    width: 774px;
+                    height: 300px;
+                    background-color: #FFF9F9;
+                    border: 3px solid #DF5F5F;
+                    border-radius: 50px;
+                    box-sizing: border-box;
+                ">
+                    <div style="
+                        position: absolute;
+                        top: 40px;
+                        left: 65px;
+                        display: flex;
+                        align-items: center;
+                        gap: 20px;
+                    ">
+                        <span style="
+                            font-family: 'Noto Looped Thai', sans-serif;
+                            font-size: 56px;
+                            color: #DF5F5F;
+                            font-weight: 500;
+                            white-space: nowrap;
+                        ">สิ่งที่กินไม่ได้</span>
+                        <svg width="56" height="56" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="12" cy="12" r="12" fill="#DF5F5F"/>
+                            <path d="M8 8L16 16M16 8L8 16" stroke="white" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                    <div style="
+                        position: absolute;
+                        top: 100px;
+                        width: 100%;
+                        display: flex;
+                        justify-content: center;
+                        gap: 10px;
+                    ">
+                        <img src="assets/zoo-feeder/food/Battery.png" style="width: 170px; height: 170px; object-fit: contain;" />
+                        <img src="assets/zoo-feeder/food/Garbage.png" style="width: 170px; height: 170px; object-fit: contain;" />
+                        <img src="assets/zoo-feeder/food/Soda.png" style="width: 170px; height: 170px; object-fit: contain;" />
                     </div>
                 </div>
                 <button id="tutorial-start-button" class="result-btn-home result-btn-home--start result-btn-home--tutorial">เริ่มเล่นเกม</button>
