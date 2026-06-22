9c84fa8bffa58b16e3852c2c9d7798a607ded21b
Kongphai Wutthichaiya <kong@localhost.localdomain>
2026-06-22 16:37:50 +0700

    feat: fix progress bar not imprement

--- src/ui/components/header-bar.js
+++ src/ui/components/header-bar.js
@@ -46,7 +46,7 @@ export function renderHeaderBar({
           <span class="gh-header-bar__summary">${escapeText(goalSummary)}</span>
 
           <div class="gh-header-bar__track"></div>
-          <div class="gh-header-bar__fill"></div>
+          <div class="gh-header-bar__fill" style="width:${fillPct}%"></div>
           <span class="gh-header-bar__progress-text">${escapeText(done)} / ${escapeText(total)}</span>
         </div>
 
